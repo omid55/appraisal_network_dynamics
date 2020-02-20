KL divergence is fixed in the utils

src/estimation_lib.py
@@ -1,5 +1,4 @@
 # Omid55
-
 import numpy as np
 import cvxpy as cp
 from collections import defaultdict
@@ -48,7 +47,7 @@ def model_builder(
         with_replication=True,
         lambdas = [0, 0.1, 1, 10, 100, 1000],
         model_func='average',
-        params={'with_constraints': True, 'n_splits': 3}):
+        params={'with_constraints': True, 'n_splits': 3, 'best_lambda': 0.1}):
     
     # For the baseline models.
     if model_func == 'average':
@@ -98,7 +97,7 @@ def model_builder(
                     params=params)[1]
         best_lambda = min(validation_errors, key=validation_errors.get)
     else:
-        best_lambda = 0.1
+        best_lambda = params['best_lambda']
     print('Training with the best lambda: {} on entire training set...'.format(best_lambda))
     if with_replication:
         print('Replicating ...')
@@ -145,12 +144,13 @@ def convex_optimization_model_func(
                 W_for_this_feature = Ws[feature_name]
             if len(data_element[feature_name].shape) == 1:
                 # If it was a vector, it makes a matrix with it.
-                p = data_element[feature_name]
-                data_element_matrix = np.row_stack([p, p, p, p])
+                # p = data_element[feature_name]
+                # data_element_matrix = np.row_stack([p, p, p, p])
+                element = np.matrix(data_element[feature_name]).T
             else:
                 # Unless it is already a matrix.
-                data_element_matrix = data_element[feature_name]
-            predicted += (data_element_matrix * W_for_this_feature)
+                element = data_element[feature_name]
+            predicted += (element * W_for_this_feature)
         return predicted
 
     # def predict(data_element, feature_names, B, Ws, is_solved=False):
@@ -184,19 +184,22 @@ def convex_optimization_model_func(
     for feature_name in feature_names:
         if len(X_train[0][feature_name].shape) == 1:
             # If it was a vector, it makes a matrix with it.
-            Ws[feature_name] = cp.Variable(
-                len(X_train[0][feature_name]), len(X_train[0][feature_name]))
             # Ws[feature_name] = cp.Variable(
-            #     len(X_train[0][feature_name]), 1)
+            #     len(X_train[0][feature_name]), len(X_train[0][feature_name]))
+            Ws[feature_name] = cp.Variable(1, len(X_train[0][feature_name]))
         else:
             # Unless it is already a matrix.
             Ws[feature_name] = cp.Variable(
-                X_train[0][feature_name].shape[1], X_train[0][feature_name].shape[0])
+                X_train[0][feature_name].shape[1],
+                X_train[0][feature_name].shape[0])
     B = cp.Variable(4, 4)
 
     # Computing loss.
     print('Computing convex loss on {} training data samples.'.format(
         len(X_train)))
+    print('Variables are:')
+    for k, w in Ws.items():
+        print('\t{}: {}'.format(k, w.size))
     constraints = []
     losses = 0
     for index in range(len(X_train)):
@@ -207,8 +210,22 @@ def convex_optimization_model_func(
         estimation_predicted = predict(
             data_element=element, feature_names=feature_names, B=B, Ws=Ws, is_solved=False)
 
-        # Defining the loss function.
+        # ------------------------------------------------------------------
+        #  ------------- Defining the loss function ------------------------
+        # ------------------------------------------------------------------
+        # L2-loss:
         loss = cp.sum_squares(estimation_predicted - estimation_groundtruth)
+
+        # # L1-loss:
+        # loss = cp.sum_entries(
+        #     cp.abs(
+        #         estimation_predicted - estimation_groundtruth))
+        # loss = cp.norm1(estimation_predicted - estimation_groundtruth) / 4
+
+        # # Bregman matrix divergence:
+        # aa_minus = estimation_predicted * np.linalg.inv(estimation_groundtruth)
+        # loss = cp.trace(aa_minus) - cp.log_det(aa_minus) - 4
+        # ------------------------------------------------------------------
 
         losses += loss
         if params['with_constraints']:
@@ -266,9 +283,15 @@ def concatinated_deep_neural_network_model_func(
     flatten_y_train = []
     for i in range(len(X_train)):
         features = X_train[i]
-        label = y_train[i][estimation_name]            
-        flatten_X_train.append(np.hstack(
-            [np.array(features[feature_name].flatten())[0] for feature_name in feature_names]))
+        label = y_train[i][estimation_name]
+        feat_list = []
+        for feature_name in feature_names:
+            if len(features[feature_name].shape) == 1:
+                feat_list.append(features[feature_name])
+            else:
+                feat_list.append(
+                    np.array(features[feature_name].flatten())[0])
+        flatten_X_train.append(np.hstack(feat_list))
         flatten_y_train.append(np.array(label.flatten())[0])
     flatten_X_train = np.array(flatten_X_train)
     flatten_y_train = np.array(flatten_y_train)
@@ -278,8 +301,14 @@ def concatinated_deep_neural_network_model_func(
     for i in range(len(X_validation_or_test)):
         features = X_validation_or_test[i]
         label = y_validation_or_test[i][estimation_name]
-        flatten_X_validation_or_test.append(np.hstack(
-            [np.array(features[feature_name].flatten())[0] for feature_name in feature_names]))
+        feat_list = []
+        for feature_name in feature_names:
+            if len(features[feature_name].shape) == 1:
+                feat_list.append(features[feature_name])
+            else:
+                feat_list.append(
+                    np.array(features[feature_name].flatten())[0])
+        flatten_X_validation_or_test.append(np.hstack(feat_list))
         flatten_y_validation_or_test.append(np.array(label.flatten())[0])
     flatten_X_validation_or_test = np.array(flatten_X_validation_or_test)
     flatten_y_validation_or_test = np.array(flatten_y_validation_or_test)
@@ -295,21 +324,21 @@ def concatinated_deep_neural_network_model_func(
             input_shape=(input_size,),
             kernel_regularizer=regularizers.l1(lambdaa),
             activity_regularizer=regularizers.l1(lambdaa)),
-#         Dropout(0.5),
+        Dropout(0.5),
         Dense(
             units=64,
             kernel_initializer='he_normal',
             activation='elu',
             kernel_regularizer=regularizers.l1(lambdaa),
             activity_regularizer=regularizers.l1(lambdaa)),
-#         Dropout(0.5),
+        Dropout(0.5),
         Dense(
             units=32,
             kernel_initializer='he_normal',
             activation='elu',
             kernel_regularizer=regularizers.l1(lambdaa),
             activity_regularizer=regularizers.l1(lambdaa)),
-#         Dropout(0.5),
+        Dropout(0.5),
         Dense(16, kernel_initializer='glorot_uniform', activation='sigmoid')])
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
@@ -369,7 +398,8 @@ def sbt_model_func(
         lambdaa=[],
         error_type_str='mse',
         params={'mode': 1}):
-    """Structural Balance Theory model inspired (similar to Kulakowski)."""
+    """Structural Balance Theory model inspired (similar to Kulakowski et 2005).
+    """
     if 'mode' in params:
         mode = params['mode']
     else:

src/pogs_jeopardy_log_lib.py
@@ -116,6 +116,7 @@ class TeamLogProcessor(object):
             game_info: Jeopardy-like game information.
             team_event_logs: All event logs for this team.
             messages: The communication messages among members.
+            members: List of members in the correct order.
     """
     def __init__(self,
                  team_id: int,
@@ -135,9 +136,9 @@ class TeamLogProcessor(object):
         self._load_game_questions(
             file_path=logs_directory_path + '/jeopardy.json')
         self._load_this_team_event_logs(
-            logs_file_path=logs_directory_path + 'event_log.csv',
+            logs_file_path=logs_directory_path + '/event_log.csv',
             team_has_subject_file_path=logs_directory_path
-                + 'team_has_subject.csv')
+                + '/team_has_subject.csv')
         self._load_messages()
         self._load_answers_chosen()
         self._load_machine_usage_info()
@@ -379,8 +380,8 @@ class TeamLogProcessor(object):
 
     def _preload_data(self, directory) -> None:
         # Preloading of the data
-        self.event_log = pd.read_csv(directory+"event_log.csv", sep=',',quotechar="|", names=["id","event_type","event_content","timestamp","completed_task_id","sender_subject_id","receiver_subject_id","session_id","sender","receiver","extra_data"])
-        self.team_subjects = pd.read_csv(directory+"team_has_subject.csv",sep=',',quotechar="|",names=["id","teamId","sender_subject_id"]).drop('id',1)
+        self.event_log = pd.read_csv(directory+"/event_log.csv", sep=',',quotechar="|", names=["id","event_type","event_content","timestamp","completed_task_id","sender_subject_id","receiver_subject_id","session_id","sender","receiver","extra_data"])
+        self.team_subjects = pd.read_csv(directory+"/team_has_subject.csv",sep=',',quotechar="|",names=["id","teamId","sender_subject_id"]).drop('id',1)
         event_log_no_message =  self.event_log[(self.event_log['event_type'] == "TASK_ATTRIBUTE")]
         event_log_no_message["sender_subject_id"] = pd.to_numeric(event_log_no_message["sender_subject_id"])
 
@@ -407,7 +408,7 @@ class TeamLogProcessor(object):
 
     def _define_team_member_order(self, directory) -> None:
         # Define teammember order
-        subjects = pd.read_csv(directory+"subject.csv", sep=',',quotechar="|", names=["sender_subject_id","externalId","displayName","sessionId","previousSessionSubject"])
+        subjects = pd.read_csv(directory+"/subject.csv", sep=',',quotechar="|", names=["sender_subject_id","externalId","displayName","sessionId","previousSessionSubject"])
         team_with_subject_details = pd.merge(self.team_subjects, subjects, on='sender_subject_id', how='left')
         self.team_member = team_with_subject_details[(team_with_subject_details['teamId'] == self.team_id)]['displayName']
         self.team_size = len(self.team_member)
@@ -523,13 +524,6 @@ class TeamLogProcessor(object):
 
     def _load_accumulated_score(self) -> None:
         """Loads the accumulated score per question"""
-        indices = [0] + list(
-            np.where(self.team_event_logs.extra_data == 'SubmitButtonField')[0])
-        begin_index = 0
-        end_index = 1
-
-        # def parse_event_content(event_content):
-
         self.score = {}
         self.accumulated_score = {}
         self.accumulated_score[0] = 0
@@ -540,6 +534,12 @@ class TeamLogProcessor(object):
             final_answer_chosen = None
             if len(set(self.group_answers_chosen[i].values())) == 1:
                 final_answer_chosen = self.group_answers_chosen[i][list(self.group_answers_chosen[i].keys())[0]]
+
+            if i not in self.game_info.questions:
+                print(
+                    'Warning: question {} was not found in the game info.'.format(i))
+                continue
+
             answer = self.game_info.questions[i].answer
 
             if (final_answer_chosen == answer):
@@ -586,15 +586,15 @@ class TeamLogProcessor(object):
         self.c = 4
         self.e = -1
         self.z = -1
-        
+
 #         Other Parameters
         self.influenceMatrixIndex = 0
         self.machineUseCount = [-1, -1, -1, -1]
         self.firstMachineUsage = [-1, -1, -1, -1]
         
         # Preloading of the data
-        eventLog = pd.read_csv(directory+"event_log.csv", sep=',',quotechar="|", names=["id","event_type","event_content","timestamp","completed_task_id","sender_subject_id","receiver_subject_id","session_id","sender","receiver","extra_data"])
-        teamSubjects = pd.read_csv(directory+"team_has_subject.csv",sep=',',quotechar="|",names=["id","teamId","sender_subject_id"]).drop('id',1)
+        eventLog = pd.read_csv(directory+"/event_log.csv", sep=',',quotechar="|", names=["id","event_type","event_content","timestamp","completed_task_id","sender_subject_id","receiver_subject_id","session_id","sender","receiver","extra_data"])
+        teamSubjects = pd.read_csv(directory+"/team_has_subject.csv",sep=',',quotechar="|",names=["id","teamId","sender_subject_id"]).drop('id',1)
         elNoMessage =  eventLog[(eventLog['event_type'] == "TASK_ATTRIBUTE")]
         elNoMessage["sender_subject_id"] = pd.to_numeric(elNoMessage["sender_subject_id"])
         
@@ -624,7 +624,7 @@ class TeamLogProcessor(object):
             self.machineAskedQuestions.append(int(float(self.machineAsked.iloc[i]['questionNumber'])))
         
         # Load correct answers
-        with open(directory+"jeopardy.json") as json_data:
+        with open(directory+"/jeopardy.json") as json_data:
                 d = json.load(json_data)
         self.correctAnswers = list()
         self.options = list()
@@ -659,7 +659,7 @@ class TeamLogProcessor(object):
                 self.groupSubmission.groupAnswer[i] = "Consensus Not Reached"
              
         # Define teammember order
-        subjects = pd.read_csv(directory+"subject.csv", sep=',',quotechar="|", names=["sender_subject_id","externalId","displayName","sessionId","previousSessionSubject"])
+        subjects = pd.read_csv(directory+"/subject.csv", sep=',',quotechar="|", names=["sender_subject_id","externalId","displayName","sessionId","previousSessionSubject"])
         teamWithSujectDetails = pd.merge(teamSubjects, subjects, on='sender_subject_id', how='left')
         self.teamMember = teamWithSujectDetails[(teamWithSujectDetails['teamId'] == teamId)]['displayName']        
         self.teamSize = len(self.teamMember)

src/pogs_jeopardy_log_lib_test.py
@@ -836,44 +836,62 @@ class TeamLogProcessorLoadRatingsTest(unittest.TestCase):
         correct_team_array = ['pogs3.2', 'pogs3.4', 'pogs3.3', 'pogs3.1']
         self.assertEqual(correct_team_array, self.loader.team_array)
 
-    def test_set_team_members(self):
+    def test_team_members_order(self):
         expected_members = [18, 20, 19, 17]
         computed_members = self.loader.members
         self.assertListEqual(expected_members, computed_members)
 
     def test_agent_ratings(self):
-        correct_agent_ratings_0 = [
-            [0.0, 0.0, 0.0, 0.0], [0.3, 0.1, 0.3, 0.3],
-            [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
-        correct_agent_ratings_1 = [
-            [2.0, 2.0, 4.0, 4.0], [4.0, 4.0, 2.0, 2.0],
-            [0.0, 0.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0]]
-        correct_agent_ratings_2 = [
-            [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0]]
-        correct_agent_ratings_3 = [
-            [0.0, 0.0, 1.0, 1.0], [0.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0]]
-
-        self.assertEqual(correct_agent_ratings_0, self.loader.agent_ratings[0])
-        self.assertEqual(correct_agent_ratings_1, self.loader.agent_ratings[1])
-        self.assertEqual(correct_agent_ratings_2, self.loader.agent_ratings[2])
-        self.assertEqual(correct_agent_ratings_3, self.loader.agent_ratings[3])
+        expected = [
+            np.array(
+                [[0.0, 0.0, 0.0, 0.0],
+                [0.3, 0.1, 0.3, 0.3],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0]]),
+            np.array(
+                [[2.0, 2.0, 4.0, 4.0],
+                [4.0, 4.0, 2.0, 2.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0]]),
+            np.array(
+                [[0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0]]),
+            np.array(
+                [[0.0, 0.0, 1.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0]])
+            ]
+        computed = self.loader.agent_ratings
+        np_testing.assert_array_equal(expected, computed)
 
     def test_member_influences(self):
-        correct_member_influences_0 = [
-            [25, 25, 25, 25], [40, 0, 40, 20], [0, 100, 0, 0], [1, 1, 1, 97]]
-        correct_member_influences_1 = [
-            [10, 20, 30, 40], [40, 30, 20, 10], [5, 20, 5, 70], [1, 1, 1, 97]]
-        correct_member_influences_2 = [
-            [25, 25, 25, 25], [25, 25, 25, 25], [10, 20, 10, 60], [1, 1, 1, 97]]
-        correct_member_influences_3 = [
-            [25, 25, 25, 25], [25, 25, 25, 25], [20, 30, 20, 30], [0, 2, 2, 96]]
-
-        self.assertEqual(correct_member_influences_0, self.loader.member_influences[0])
-        self.assertEqual(correct_member_influences_1, self.loader.member_influences[1])
-        self.assertEqual(correct_member_influences_2, self.loader.member_influences[2])
-        self.assertEqual(correct_member_influences_3, self.loader.member_influences[3])
+        expected = [
+            np.array(
+                [[25, 25, 25, 25],
+                [40, 0, 40, 20],
+                [0, 100, 0, 0],
+                [1, 1, 1, 97]]),
+            np.array(
+                [[10, 20, 30, 40],
+                [40, 30, 20, 10],
+                [5, 20, 5, 70],
+                [1, 1, 1, 97]]),
+            np.array(
+                [[25, 25, 25, 25],
+                [25, 25, 25, 25],
+                [10, 20, 10, 60],
+                [1, 1, 1, 97]]),
+            np.array(
+                [[25, 25, 25, 25],
+                [25, 25, 25, 25],
+                [20, 30, 20, 30],
+                [0, 2, 2, 96]])
+            ]
+        computed = self.loader.member_influences
+        np_testing.assert_array_equal(expected, computed)
 
     def test_agent_ratings_from_data(self):
         correct_agent_ratings_from_data_0 = [
@@ -1099,3 +1117,43 @@ class TeamLogProcessorSurveyTestForTeam11(unittest.TestCase):
             correct_survey_answers[2])
         self.assertDictEqual(self.loader.pre_experiment_rating[3],
             correct_survey_answers[3])
+
+# =========================================================================
+# ================== TeamLogProcessor's __init__ ==========================
+# =========================================================================
+class TeamLogProcessorInit(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open(TESTING_LOG_FILE_PATH, 'w') as f:
+            f.writelines(TESTING_LOG)
+        with open(TESTING_TEAM_HAS_SUBJECT_FILE_PATH, 'w') as f:
+            f.writelines(TESTING_TEAM_HAS_SUBJECT)
+        with open(TESTING_JEOPARDY_FILE_PATH, 'w') as f:
+            f.writelines(TESTING_JEOPARDY_JSON)
+        with open(TESTING_SUBJECT_PATH, 'w') as f:
+            f.writelines(TESTING_SUBJECT)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(TESTING_LOG_FILE_PATH)
+        os.remove(TESTING_JEOPARDY_FILE_PATH)
+
+    def test_init(self):
+        loader = lib.TeamLogProcessor(
+            team_id=7, logs_directory_path='/tmp')
+        np_testing.assert_array_equal(
+            loader.member_influences,
+            np.array(
+                [[[25, 25, 25, 25],
+                [40, 0, 40, 20],
+                [0, 100, 0, 0],
+                [1, 1, 1, 97]]]))
+        individual_responses = loader.individual_answers_chosen
+        group_responses = loader.group_answers_chosen
+        self.assertEqual(len(individual_responses), 6)
+        self.assertEqual(len(group_responses), 6)
+        self.assertEqual(individual_responses[41] ==
+            {17: 'Chicken Little', 18: 'Chicken Little',
+            19: 'Chicken Little', 20: 'Chicken Little'}, True)
+            

src/utils.py
@@ -9,6 +9,7 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 from itertools import permutations
 import pandas as pd
 import numpy as np
+import scipy as sp
 import matplotlib
 import matplotlib.pyplot as plt
 import pickle as pk
@@ -661,8 +662,8 @@ def shuffle_matrix_in_given_order(matrix: np.ndarray,
 
 
 def replicate_matrices_in_train_dataset_with_reordering(
-    X_train:List[Dict],
-    y_train:List[Dict],
+    X_train: List[Dict],
+    y_train: List[Dict],
     matrix_string_name = 'influence_matrix') -> Tuple[List[Dict], List[Dict]]:
     """Replicates matrices in the training dataset to have all orders of nodes.
     
@@ -720,7 +721,7 @@ def matrix_estimation_error(
     neg_corr: Negative correlation of vectorized matrices if stat significant.
     cosine_dist: Cosine distance of vectorized matrices from each other.
     l1: L1-norm distance in each row (since they are row-stochastic).
-    kl_divergence: 
+    kl: KL divergence in every row of the row-stochastic matrix.
     
     Args:
         true_matrix: The groundtruth matrix.
@@ -764,20 +765,40 @@ def matrix_estimation_error(
         err = cosine(
             np.array(true_matrix.flatten()), np.array(pred_matrix.flatten()))
         return err
-    # # Distribution-based error metrics:
-    # elif type_str == 'kl':
-    #     err = 0
-    #     for i in range(n):
-    #         for j in range(m):
-    #             err += true_matrix[i, j] * (np.log2(
-    #                 true_matrix[i, j]) - np.log2(pred_matrix[i, j]))
-    #     err /= n
-    #     return err
     # L1-norm distance in each row (since they are row-stochastic).
     elif type_str == 'l1':
-        return np.mean(
-            [np.linalg.norm(true_matrix[i, :] - pred_matrix[i, :], 1)
-            for i in range(n)])
+        return np.sum(abs(true_matrix - pred_matrix)) / n
+        # Which is the same as:
+        # return np.mean(
+        #     [np.linalg.norm(true_matrix[i, :] - pred_matrix[i, :], 1)
+        #     for i in range(n)])
+    # Distribution-based error metrics:
+    elif type_str == 'kl':
+        # The definition of KL divergence uses the following conventions
+        # (see Cover and Thomas, Elements of Information Theory):
+        # 0 * log(0 / 0) = 0, 0 * log(0 / q = 0, p * log(p / 0) = \infinity.
+        err = 0
+        for i in range(n):
+            for j in range(m):
+                err += sp.special.kl_div(true_matrix[i, j], pred_matrix[i, j])
+                # if true_matrix[i, j] > 0:
+                #     if pred_matrix[i, j] == 0:
+                #         err += 1000  # instead of becoming nan. << CHECK HERE >>
+                #     else:
+                #         err += true_matrix[i, j] * (
+                #             np.log2(true_matrix[i, j]) - np.log2(pred_matrix[i, j]))
+        err /= n
+        return err
+    # elif type_str == 'cross_entropy':
+    #     eps = 0.01
+    #     err = 0
+    #     for i in range(n):
+    #         if any(pred_matrix[i, :] == 0):
+    #             pred_matrix[i, :] += eps
+    #         for j in range(m):
+    #             err -= true_matrix[i, j] * np.log2(pred_matrix[i, j])
+    #     err /= n
+    #     return err 
     else:
         raise ValueError('Wrong type_str was given, which was: {}'.format(
             type_str))
@@ -860,7 +881,8 @@ def compute_relationship(
         # Scatter plot.
         f = plt.figure()
         sns.scatterplot(v2, v1)
-        plt.plot((min(v1), max(v2)), (max(v1), min(v2)), 'r')
+        # plt.plot((min(v1), max(v2)), (max(v1), min(v2)), 'r')
+        plt.plot(np.linspace(min(v2), max(v2)), np.linspace(min(v1), max(v1)), 'r')
         plt.xlabel(v2_label)
         plt.ylabel(v1_label)
         plt.show()
@@ -874,3 +896,121 @@ def compute_relationship(
         maxlag=maxlag,
         verbose=verbose)
     return {'rval': rval, 'pval': pval, 'causality': causality_res}
+
+
+# @enforce.runtime_validation
+def _get_eigen_decomposition_of_markov_transition(
+        transition_matrix: np.ndarray,
+        aperiodic_irreducible_eps: float = 0.0001) -> Tuple:
+    """Gets the eigen value and vectors from transition matrix.
+
+    A Markov chain is irreducible if we can go from any state to any state.
+    This entails all transition probabilities > 0.
+    A Markov chain is aperiodic if all states are accessible from all other
+    states. This entails all transition probabilities > 0.
+
+    Args:
+        transition_matrix: Square Markov transition matrix.
+
+        aperiodic_irreducible_eps: To make the matrix aperiodic/irreducible.
+
+    Returns:
+        Dictionary of eigen val/vec of irreducible and aperiodic markov chain.
+
+    Raises:
+        ValueError: If the matrix was not squared.
+    """
+    if transition_matrix.shape[0] != transition_matrix.shape[1]:
+        raise ValueError('Transition matrix is not squared.')
+    matrix = transition_matrix.copy()
+    matrix = np.nan_to_num(matrix)
+    matrix += aperiodic_irreducible_eps
+    aperiodic_irreducible_transition_matrix = (
+        matrix.T / np.sum(matrix, axis=1)).T
+    eigen_values, eigen_vectors = np.linalg.eig(
+        aperiodic_irreducible_transition_matrix.T)
+    return eigen_values, eigen_vectors
+
+
+# @enforce.runtime_validation
+def get_stationary_distribution(
+        transition_matrix: np.ndarray,
+        aperiodic_irreducible_eps: float = 0.0001) -> np.ndarray:
+    """Gets the stationary distribution of given transition matrix.
+
+    A Markov chain is irreducible if we can go from any state to any state.
+    This entails all transition probabilities > 0.
+    A Markov chain is aperiodic if all states are accessible from all other
+    states. This entails all transition probabilities > 0.
+
+    Args:
+        transition_matrix: Square Markov transition matrix.
+
+        aperiodic_irreducible_eps: To make the matrix aperiodic/irreducible.
+
+    Returns:
+        Array of size one dimension of matrix.
+
+    Raises:
+        ValueError: If the matrix was not squared.
+    """
+    eigen_values, eigen_vectors = (
+        _get_eigen_decomposition_of_markov_transition(
+            transition_matrix=transition_matrix,
+            aperiodic_irreducible_eps=aperiodic_irreducible_eps))
+    index = np.where(eigen_values > 0.99)[0][0]
+    stationary_distribution = [item.real for item in eigen_vectors[:, index]]
+    stationary_distribution /= np.sum(stationary_distribution)
+    return stationary_distribution
+
+
+def get_relative_reflected_appraisal_matrix(
+        influence_matrix: np.ndarray) -> np.ndarray:
+    """Gets relative reflected appraisal matrix.
+    
+    Relative reflected appraisal matrix is the zero-diagonaled influence matrix
+    after normalizing. It shows how much one is getting influenced by others
+    regardless of him or herself. In the derivtion, we call this matrix C.
+    
+    Args:
+        influence_matrix: row stochastic positive influence matrix.
+
+    Returns:
+        The relative reflected appraisal matrix.
+
+    Raises:
+        None.
+    """
+    influence_matrix = np.array(influence_matrix)
+    matrix = influence_matrix.copy()
+    np.fill_diagonal(matrix, 0)
+    matrix = np.nan_to_num(matrix.T / np.sum(matrix, axis=1)).T
+    return matrix
+
+
+def get_average_influence_on_others(
+        influence_matrix: np.ndarray, index: int) -> float:
+    """Gets average noramlized influence on others.
+    
+    \frac{1}{n-1}\sum\limits_{j \neq i} influence_matrix_{ji}
+
+    Args:
+        influence_matrix: row-stochastic positive influence matrix.
+
+        index: The index of individual to return their influence on others.
+
+    Returns:
+        The relative reflected appraisal matrix.
+
+    Raises:
+        ValueError: If index was outside of [0, n-1].
+    """
+    n, _ = influence_matrix.shape
+    if index < 0 or index >= n:
+        raise ValueError(
+            'Index was outside of boundary [0, {}]. It was {}'.format(
+                n-1, index))
+    influence_matrix = np.array(influence_matrix)
+    return np.mean(
+        influence_matrix[[i for i in range(n)
+           if i != index], index])